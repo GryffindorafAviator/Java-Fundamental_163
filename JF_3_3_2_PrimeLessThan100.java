9386f2f4b0818e734099ee5ab098324867a1e612
Flash_W <phynew@hotmail.com>
2017-04-18 22:19:20 +0800

    Add files via upload

---
+++ JF_3_3_2_PrimeLessThan100.java
@@ -0,0 +1,34 @@
+package hello;
+
+import java.util.Scanner;
+
+public class JF_3_3_2_PrimeLessThan100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		for ( int n = 2; n < 100; n++)
+		{
+		    int isPrime = 1;
+			for (int i = 2; i < n; i++)
+			{
+				if ( n % i == 0)
+				{
+					isPrime = 0;
+					break;
+				}
+			}
+			
+			if ( isPrime == 1)
+			{
+				System.out.print(n + " ");
+			}
+			else
+			{
+//				System.out.println(n + "²»ÊÇËØÊý");
+			}
+		}
+	}
+
+}
