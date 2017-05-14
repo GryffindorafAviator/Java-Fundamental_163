9dd6b3cc1eb033e8dfd115693f672f5508cb84e5
Flash_W <phynew@hotmail.com>
2017-05-14 11:23:58 +0800

    Create 2 Methods to Find Primes.java

---
+++ 2 Methods to Find Primes.java
@@ -0,0 +1,26 @@
+// 1, eliminate even numbers, number of cycles is approximate to n/2
+if(x==1 || x%2==0 && x!=2)
+		{
+			isPrime = false;
+		}
+		else
+		{
+			for(int 1=3; i<x; i+=2)
+			{
+				if(x%i == 0)
+				{
+					isPrime = false;
+					break;
+				}
+			}
+		}
+    
+//2, to sqrt(x), number of cycles is sqrt(x)
+for(int i=3; i<Math.sqrt(x); i+=2)
+		{
+			if(x%i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
