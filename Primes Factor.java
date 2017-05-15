9227ef046996f659d8bcaaaf9c2c19e56001ed8d
Flash_W <phynew@hotmail.com>
2017-05-15 12:36:54 +0800

    Update Primes Factor.java

--- Primes Factor.java
+++ Primes Factor.java
@@ -11,15 +11,20 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		
 		int integer = in.nextInt();
+		int a = integer;
 		int j = 0;
-		int[] primes = new int[1000];
+		String output = "";
+		int[] primes = new int[100000];
 		int[] primeFactor = new int[integer];
-		boolean[] isPrime = new boolean[1000];
+		boolean[] isPrime = new boolean[primes.length];
+		
+//		set each cell of array isPrime to true
 		for(int i=0; i<isPrime.length; i++)
 		{
 			isPrime[i] = true;
 		}
 		
+//		construct primes table
 		for(int i=2; i<isPrime.length; i++)
 		{
 			if(isPrime[i])
@@ -31,11 +36,16 @@ public class Main {
 			}
 		}
 		
-		
+//		find out prime factors of arbitrary integer
 		for(int i=2; i<=integer; i++)
 		{
 			if(isPrime[i])
 			{
+				if(integer == i)
+				{
+					primeFactor[j] = i;
+					break;
+				}
 				if(integer%i == 0)
 				{
 					primeFactor[j] = i;
@@ -45,17 +55,23 @@ public class Main {
 				}
 			}
 		}
-		
-//		for(int i=2; i<isPrime.length; i++)
-//		{
-//			if(isPrime[i])
-//			{
-//				primes[i] = i;
-//			}
-//		}
-		
-		
-		
-		System.out.println(Arrays.toString(primeFactor));
+
+		for(int i=0; i<primeFactor.length; i++)
+		{
+			if(primeFactor[i]!=0)
+			{
+				if(i==0)
+				{
+					output = primeFactor[i] + "";
+				}
+				else if(i>0)
+				{
+					output = output + "x" + primeFactor[i];
+				}
+			}
+		}
+//		System.out.println(Arrays.toString(primeFactor));
+		System.out.println(a + "=" + output);
 	}
 }
+
