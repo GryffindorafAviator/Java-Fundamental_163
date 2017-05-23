2dcff949900a8ed2958034f8be5d9239ce45f10a
Flash_W <phynew@hotmail.com>
2017-05-23 23:54:22 +0800

    Update Class Vending_Machine.java

--- Class Vending_Machine.java
+++ Class Vending_Machine.java
@@ -5,6 +5,17 @@ public class Vending_Machine {
 	int balance;
 	int total;
 	
+	Vending_Machine()
+	{
+		total = 0;
+	}
+	
+	Vending_Machine(int price)
+	{
+		this();       //this method can call other construction function, but it can only be used inside the construction and must the first clause of the construction, and it can be used only once 
+		this.price = price;
+	}
+	
 	void showPrompt()
 	{
 		System.out.println("Welcome");
@@ -38,6 +49,10 @@ public class Vending_Machine {
 		vm.insertMoney(100);
 		vm.getFood();
 		vm.showBalance();
+		Vending_Machine vm1 = new Vending_Machine(100);
+		vm1.insertMoney(200);
+		vm.showBalance();
+		vm1.showBalance();
 	}
 
 }
